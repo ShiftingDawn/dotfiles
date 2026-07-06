446c6aef6376829295f602c27db0cca91803b59e
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2026-07-06 17:56:41 +0200

    Change wallpaper and border colors

--- .config/hypr/look_and_feel.lua
+++ .config/hypr/look_and_feel.lua
@@ -1,11 +1,11 @@
 hl.config({
   general = {
     gaps_in  = 5,
-    gaps_out = 20,
+    gaps_out = 5,
     border_size = 2,
     col = {
-      active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
-      inactive_border = "rgba(595959aa)",
+      active_border   = { colors = {"rgba(b4befeee)", "rgba(eba0acee)"}, angle = 45 },
+      inactive_border = "rgba(6c7086aa)",
     },
     resize_on_border = false,
     allow_tearing = false,
